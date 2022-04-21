8ded0a54349a43ed6eaea12b981f17a0b38795d8
Damian Krysta <damian@krysta.dev>
2022-04-21 23:46:11 +0200

    INIT and directory select
    Signed-off-by: Damian Krysta <damian@krysta.dev>

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ElectronService } from './core/services';
+import { TranslateService } from '@ngx-translate/core';
+import { APP_CONFIG } from '../environments/environment';
+import {dialog} from 'electron';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private electronService: ElectronService,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('en');
+    console.log('APP_CONFIG', APP_CONFIG);
+
+    if (electronService.isElectron) {
+      console.log(process.env);
+      console.log('Run in electron');
+      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
+      console.log('NodeJS childProcess', this.electronService.childProcess);
+    } else {
+      console.log('Run in browser');
+    }
+  }
+
+  selectDirectory() {
+    if (this.electronService.isElectron) {
+      console.log('send')
+      this.electronService.ipcRenderer.send('select-directory', '')
+
+      this.electronService.ipcRenderer.on('selected-directory', (event, args) => {
+        console.log(args)
+      })
+    }
+  }
+}
